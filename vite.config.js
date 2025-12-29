f8e0c982b5ef1efaa2f52be0fb0f33ec45222e14
selsabil-dev <boukaousselsabil00@gmail.com>
2025-12-29 15:06:47 +0100

    S

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react/jsx-runtime', 'react/jsx-dev-runtime']
+  },
+  server: {
+    port: 3001,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
 })
